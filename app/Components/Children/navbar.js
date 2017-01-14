dc0366d8f307f18f60ec8415c311bf4e1c2be82a
Joshua Hudson <joshudson93@gmail.com>
2017-01-14 11:32:11 -0500

    Tossing up package.json

---
+++ app/Components/Children/navbar.js
@@ -0,0 +1,26 @@
+var React = require('react');
+   
+var Nav = React.createClass({
+
+  return(
+
+    <nav id="mainNav" className="navbar navbar-default navbar-fixed-top navbar-custom">
+        <div className="container">
+           
+            <div className="navbar-header page-scroll">
+                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span className="sr-only">Toggle navigation</span> Menu <i className="fa fa-bars"></i>
+                </button>
+                <a className="navbar-brand" href="#page-top">My Account</a>
+            </div>
+
+        
+         
+        </div>
+        
+    </nav>
+   )
+});
+
+module.exports = Nav;
+
---
+++ app/Components/Main.js
@@ -0,0 +1,20 @@
+var React = require("react");
+
+var Main = React.createClass({
+
+render: function(){
+	return(
+  
+         <div className="container">
+          <div className="row">
+
+        
+          </div>
+          </div>
+		)
+}
+
+
+});
+
+module.exports = Main;
