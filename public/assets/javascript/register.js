b77f99b86405817a6136f6169ef62eb0c3f120bf
Ben Cheng <bencheng00@gmail.com>
2017-01-14 12:53:05 -0500

    added music

--- public/assets/javascript/register.js
+++ public/assets/javascript/register.js
@@ -1,44 +1,16 @@
 
-
-var debbie = document.createElement('audio');
-	       debbie.setAttribute('src', "assets/javascript/debbie.mp3");
-
-var tribute = document.createElement('audio');
-	       tribute.setAttribute('src', "assets/javascript/tribute.mp3");
+var requiem = document.createElement('audio');
+	       requiem.setAttribute('src', "assets/images/requiem.mp3");
 
 
 $(document).ready(function(){
 
 
-	debbie.play();
-	debbie.addEventListener('ended', function() {
+	requiem.play();
+	requiem.addEventListener('ended', function() {
    		this.currentTime = 0;
     	this.play();
 		}, false);
-
-
-
-function katnissgone(){
-	$("#screen2").css("display","block");
-	$("body").css("background-image","url(assets/css/background.jpg");
-	$("body").css("background-size","auto");
-};
-
-function katniss(){
-	$("#screen2").css("display","none");
-	$("body").css("background-image","url(assets/css/katniss.jpg");
-	$("body").css("background-size","100%");
-	tribute.play();
-	setTimeout(katnissgone,1500);
-};
-
-function katnissloop(){
-	katniss();
-	setTimeout(katnissloop,120000);
-};
-
-
-setTimeout(katnissloop,5000)
 
 
 });
