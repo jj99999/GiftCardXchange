dd7d5b82a8d7cf63862de7e709f930f6b154f8d3
jj99999 <jonathan.jackson3@gmail.com>
2017-01-14 11:24:44 -0500

    edited server file to add all the routes and added controllers

---
+++ controllers/auth.js
@@ -0,0 +1,12 @@
+var models = require('../models');
+var express = require('express');
+var router = express.Router();
+
+// need to know the right name for this auth route
+router.get('/', function(req, res){
+	// insert  passport auth code here
+	
+	// res.sendFile('../public/index.html');
+});
+
+module.exports = router; 
---
+++ controllers/buyCard.js
@@ -0,0 +1,12 @@
+var models = require('../models');
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res){
+	
+	// insert link to buy Card html
+	// res.sendFile('../public/index.html');
+
+});
+
+module.exports = router; 
