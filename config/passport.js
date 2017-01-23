21a6d21f2777c57b5710f340251d8b943762c9ae
jj99999 <jonathan.jackson3@gmail.com>
2017-01-23 18:56:52 -0500

    created currentUserEmail variable

--- config/passport.js
+++ config/passport.js
@@ -110,8 +110,11 @@ module.exports = function(passport) {
                 return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
 
             // all is well, return successful user
+
             var currentUserEmail = user.local.email;
             console.log("The current user is "+currentUserEmail);
+            // all is well, return successful user
+
             return done(null, user);
         });
 
