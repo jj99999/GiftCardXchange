baf7f7a3af741f2445f457b7da0cdd7207cd31dc
Jonathan Jackson <jj99999@users.noreply.github.com>
2017-01-23 18:58:00 -0500

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -109,6 +109,9 @@ module.exports = function(passport) {
             if (!user.validPassword(password))
                 return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
 
+            var currentUserEmail = user.local.email;
+            console.log("The current user is "+currentUserEmail);
+            
             // all is well, return successful user
             return done(null, user);
         });
