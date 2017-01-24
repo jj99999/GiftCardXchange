789278ab5155532d2e5c0ef403a2fdf53fa2656b
Joshua Hudson <joshudson93@gmail.com>
2017-01-23 19:15:24 -0500

    COmponentMount

--- app/Components/utils/helpers.js
+++ app/Components/utils/helpers.js
@@ -1,12 +1,19 @@
 // Include the axios package for performing HTTP requests (promise based alternative to request)
-var axios = require('axios');
+// var axios = require('axios');
+// var express = require('express');
+// var app = express();
 
+// var passport = require('passport');
+// var passportLocal   = require('passport-local');
 
-var helper = {
+// var passport = require ('../config/passport.js');
+
+// var helper = {
 
    
-};
+
+// };
 
 
-// We export the helpers function 
-module.exports = helper;
+// // We export the helpers function 
+// module.exports = helper;
