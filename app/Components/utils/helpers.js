11f355282b67de909036fd18ebc377a54aaabd43
Joshua Hudson <joshudson93@gmail.com>
2017-01-25 19:43:34 -0500

    Axios console log of user email

--- app/Components/utils/helpers.js
+++ app/Components/utils/helpers.js
@@ -1,9 +1,21 @@
 // Include the axios package for performing HTTP requests (promise based alternative to request)
 var axios = require('axios');
 
+var email;
+
 var helper = {
 
         
+getUser: function(){
+	return axios.get("/login").then(function(response){
+		console.log("HERE IS AXIOS EMAIL");
+	   email = response;
+		console.log(email);
+	})
+
+}
+
+
 };
 
 
