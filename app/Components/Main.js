de777004f57dd92786035712ff3722f39fead4fa
jj99999 <jonathan.jackson3@gmail.com>
2017-01-27 20:37:43 -0500

    inventory updates

--- app/Components/Main.js
+++ app/Components/Main.js
@@ -11,23 +11,70 @@ var helpers = require("./utils/helpers");
 
 // This is the main component
 var Main = React.createClass({
-getInitialState: function(){
-
-
-    return { email:""};
+ getInitialState: function() {
+    return { userCards: []};
   },
 
+   setUser: function(user) {
+    this.setState({ userCards: user});
+  },
 
-
-  //   },
+// setEmail: function(email){
+//   this.setState({email: email });
+// },
+ //    return {
+    
+ //    cards: [{
+ //    "StoreName": "Walmart",
+ //    "CardBalance": "$100",
+ //    "RedemptionCode": "123456789",
+ //    "RemoveCard": "Blah"
+ //  }, 
+ // {
+ //    "StoreName": "Lesbian Pottert",
+ //    "CardBalance": "$200",
+ //    "RedemptionCode": "987654321",
+ //    "RemoveCard": "Blah"
+ //  }, 
+ // {
+ //    "StoreName": "Home Depot",
+ //    "CardBalance": "$100",
+ //    "RedemptionCode": "337879657",
+ //    "RemoveCard": "Blah"
+ //  }, 
+ // {
+ //    "StoreName": "Apple",
+ //    "CardBalance": "$50",
+ //    "RedemptionCode": "657890234",
+ //    "RemoveCard": "Blah"
+ //  }, 
+ //  {
+ //    "StoreName": "StarBucks",
+ //    "CardBalance": "$150",
+ //    "RedemptionCode": "9318790562",
+ //    "RemoveCard": "Blah"
+ //  }] 
+ //    }
+   
   componentDidMount: function(){
    helpers.getUser().then(function(response){
-      var email = response;
-      // console.log("HERE IS AXIOS EMAIL FROM PARENT");
-      // console.log(email);
-      }.bind(this))
 
-   // helpers.allCards();
+    // this.setState({email: response.data});
+    // var email = response;
+    //  console.log("HERE IS AXIOS EMAIL FROM PARENT");
+    //  console.log(email)
+   }.bind(this));
+
+   helpers.getInventory().then(function(response){
+console.log("MAIN getInventory");
+console.log(response.data);
+this.setState({ userCards : response.data });
+
+       
+
+// userCard = response;
+// cookie.save('userCard', userCard);
+}.bind(this));
    
   },
   // Here we describe this component's render method
@@ -36,22 +83,21 @@ getInitialState: function(){
       <div className="container" id="screen2">
             <div className="row">
              <div className="col-md-12">
-            <Inventory cards={this.state.cards}/>
+            <Inventory cards={this.state.userCards}/>
             </div>
            </div>
 
            <div className="row">
+           <div className="col-md-4">
 
-          
+           </div>
+           <div className="col-md-4">
+           <AddCard />
+    
+           </div>
+           <div className="col-md-4">
 
-            <div className="col-md-6">
-            <AddCard />
-            </div>
-           
-            <div className="col-md-6">
-            <SearchCard />
-            </div>
-           
+           </div>
 
            </div>
       </div>
@@ -61,7 +107,7 @@ getInitialState: function(){
 
     );
 
-  }
+  },
 
 
 });
