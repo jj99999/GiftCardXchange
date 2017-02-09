c6fa351c7d2957f24dd25de66314f87f25c49d67
Joshua Hudson <joshudson93@gmail.com>
2017-02-09 14:59:10 -0500

    Removing all soundtracks for a second

---
+++ app/Components/GiftCard.js
@@ -0,0 +1,152 @@
+import React from 'react';
+import darkBaseTheme from '../../node_modules/material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from '../../node_modules/material-ui/styles/MuiThemeProvider';
+import getMuiTheme from '../../node_modules/material-ui/styles/getMuiTheme';
+import AppBar from '../../node_modules/material-ui/AppBar';
+// import MobileTearSheet from '../../node_modules/material-ui/MobileTearSheet';
+import Avatar from '../../node_modules/material-ui/Avatar';
+import {List, ListItem} from '../../node_modules/material-ui/List';
+import Subheader from '../../node_modules/material-ui/Subheader';
+import Divider from '../../node_modules/material-ui/Divider';
+import CommunicationChatBubble from '../../node_modules/material-ui/svg-icons/communication/chat-bubble';
+import ActionSwapHoriz from '../../node_modules/material-ui/svg-icons/action/swap-horiz';
+import ActionSwapVert from '../../node_modules/material-ui/svg-icons/action/swap-vert';
+import ActionCardGiftCard from '../../node_modules/material-ui/svg-icons/action/card-giftcard';
+// import FontIcon from 'material-ui/FontIcon';
+// import Avatar from '../../node_modules/material-ui/Avatar';
+import MobileTearSheet from '../MobileTearSheet';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import { Link } from 'react-router';
+
+const iconStyles = {
+	
+    color: 'white',
+
+};
+
+const style = {
+  height: 36,
+  margin: 12,
+};
+
+const position = {
+  position: 'fixed',
+}
+
+
+const GiftCard = () => (
+	<div>
+  
+    <AppBar 
+    className="navvy"
+
+    style={position}
+
+    title="Gift Card Exchange"
+   
+    children={
+
+      <div>
+      <FlatButton  label="Log In" secondary={true} style={style.margin}/>
+      <Link to="/signup"> <RaisedButton label="Sign Up" secondary={true}  style={style}  /></Link>
+
+     </div>
+  }
+   
+    />
+ 
+  <header id="top" className="header"> 
+    <div className="text-vertical-center">
+     <div className="container">
+     <div className="col-md-12">
+     <h1 className="hatch">The Virtual Card Marketpace</h1>
+     </div>
+     </div>
+
+  
+    </div>
+   </header>
+
+  <section id="services" className="services bg-primary">
+  <div className="container">
+    <div className="row">
+      <div className="col-md-6 ">
+     <h2 className="add">Keep track of any gift card</h2>
+
+ <h4>Have a holiday gift card to a store you never shop at? Wouldn't it be nice to find a marketplace where you can get something you want? Oh wait, you can!</h4>
+      </div>
+      <div className="col-md-3">
+
+      <MobileTearSheet>
+    <List>
+    <ListItem
+      disabled={true}
+      primaryText="Grace"
+      leftAvatar={<Avatar src="http://www.material-ui.com/images/uxceo-128.jpg" />}
+      rightIcon={<ActionCardGiftCard
+      />}
+   />
+   <ListItem
+      disabled={true}
+      primaryText="Brendan"
+      leftAvatar={<Avatar src="http://www.material-ui.com/images/ok-128.jpg" />}
+      rightIcon={<ActionCardGiftCard
+      />}
+
+   />
+   </List>
+   </MobileTearSheet>
+
+      </div>
+     <div className="col-md-3">
+
+      </div>
+    <div className="row">
+    <div className="col-md-6">
+
+    <h2>Exchange for gift cards you don't want for people who do want them.</h2>
+  
+    <h4 className="exchange">Go to the marketplace, take unwanted your unwanted cards, and get the ones that you've secretly always wanted but would never tell your gift givers.</h4>
+    
+   </div>
+    <div className="col-md-3">
+
+    <MobileTearSheet>
+    <List>
+    <ListItem
+      disabled={true}
+      primaryText="Grace"
+      leftAvatar={<Avatar src="http://www.material-ui.com/images/uxceo-128.jpg" />}
+      // rightIcon={<ActionCardGiftCard
+      // />}
+   />
+<ListItem
+      disabled={true}
+     leftIcon={<ActionSwapVert/>}
+      
+   />
+
+   <ListItem
+      disabled={true}
+      primaryText="Brendan"
+      leftAvatar={<Avatar src="http://www.material-ui.com/images/ok-128.jpg" />}
+   />
+   </List>
+   </MobileTearSheet>
+    </div>
+
+    <div className="col-md-3">
+
+    </div>
+ </div>
+
+  </div>
+ </div>
+  </section>
+
+    </div>
+
+);
+
+export default GiftCard;
---
+++ app/Components/SignUp.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import darkBaseTheme from '../../node_modules/material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from '../../node_modules/material-ui/styles/MuiThemeProvider';
+import getMuiTheme from '../../node_modules/material-ui/styles/getMuiTheme';
+// import AppBar from '../../node_modules/material-ui/AppBar';
+// // import MobileTearSheet from '../../node_modules/material-ui/MobileTearSheet';
+// import Avatar from '../../node_modules/material-ui/Avatar';
+import {List, ListItem} from '../../node_modules/material-ui/List';
+import Subheader from '../../node_modules/material-ui/Subheader';
+import Divider from '../../node_modules/material-ui/Divider';
+// import CommunicationChatBubble from '../../node_modules/material-ui/svg-icons/communication/chat-bubble';
+// import ActionSwapHoriz from '../../node_modules/material-ui/svg-icons/action/swap-horiz';
+// import ActionSwapVert from '../../node_modules/material-ui/svg-icons/action/swap-vert';
+// import ActionCardGiftCard from '../../node_modules/material-ui/svg-icons/action/card-giftcard';
+// // import FontIcon from 'material-ui/FontIcon';
+// // import Avatar from '../../node_modules/material-ui/Avatar';
+// import MobileTearSheet from '../MobileTearSheet';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import { Link } from 'react-router';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import TextField from 'material-ui/TextField';
+
+
+const style = {
+
+	color: 'teal',
+	marginRight: 98,
+}
+
+const SignUp = () => (
+
+
+ <Card className="container">
+ <form className="create-form" action="/signup" method="POST">
+  
+
+<div className="row">
+ <div className="col-md-4">
+
+ </div>
+
+<div className="col-md-4">
+
+<h1 className="card-heading" style={style}>Sign Up</h1>
+ <div className="field-line">
+        <TextField
+          floatingLabelText="First Name"
+          name="name"
+          // errorText={errors.name}
+          // onChange={onChange}
+          // value={user.name}
+        />
+      </div>
+
+ <div className="field-line">
+        <TextField
+          floatingLabelText="Last Name"
+          name="name"
+          // errorText={errors.name}
+          // onChange={onChange}
+          // value={user.name}
+        />
+      </div>
+
+
+<div className="field-line">
+        <TextField
+          floatingLabelText="Email"
+          name="email"
+          // errorText={errors.email}
+          // onChange={onChange}
+          // value={user.email}
+        />
+      </div>
+
+<div className="field-line">
+        <TextField
+          floatingLabelText="Password"
+          type="password"
+          name="password"
+          // onChange={onChange}
+          // errorText={errors.password}
+          // value={user.password}
+        />
+      </div>
+
+
+ <div className="button-line">
+        <RaisedButton type="submit" label="Create New Account" primary />
+      </div>
+
+        <CardText>Already have an account? <Link to={'/login'}>Log in</Link></CardText>
+  </div>
+ <div className="col-md-4">
+
+
+ </div>
+ </div>
+
+  </form>
+ </Card>
+
+
+	);
+
+export default SignUp;
